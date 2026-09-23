632674b4305be19b0b764a2afefbdcb9b085de14
Stanislav <stas.zhegest@gmail.com>
2026-09-23 18:18:12 +0200

    fixat bugs tillagda kommentarer

--- script.js
+++ script.js
@@ -4,21 +4,22 @@ let overlay;
 let closeBtn;
 let logInForm;
 let message;
+let formsMessage;
 const users = [];
 let currentUser;
 
+// Konstruktor för användare med skydd mot ändringar (fryser objektet)
 function User(newName, newPassword) {
   this.name = newName;
   this.password = newPassword;
   Object.freeze(this);
 }
 
+// Skapar nya användare och plasera i användarlistan
 function newUser(currentName, currentPass) {
   const user = new User(currentName, currentPass);
   users.push(user);
 }
-
-
 
 function showLogInWindows() {
   overlay.classList.remove("hidden");
@@ -30,10 +31,12 @@ function hideLogInWindows() {
   overlay.classList.add("hidden");
   logInWindow.classList.add("hidden");
   logInForm.reset();
+  formsMessage.textContent = "";
 }
 
 function logOut() {
   localStorage.removeItem("user");
+  currentUser = null;
   message.textContent = `Du har loggat ut.`;
   logInBtn.textContent = "Logga in";
 }
@@ -41,21 +44,26 @@ function logOut() {
 function logIn() {
   let name = logInForm.elements.name.value;
   let password = logInForm.elements.password.value;
-  let formsMessage = logInForm.querySelector("span");
+
+  // Söker användaren i listan och sparar namnet efter matchning
   users.forEach((element) => {
     if (element.name == name && element.password == password) {
       localStorage.setItem("user", element.name);
     }
   });
+
+  // Kontrollerar om inloggningen lyckades
   if (!isLoggedIn()) {
     formsMessage.textContent = "Felaktiga inloggningsuppgifter";
     return;
   }
+
   hideLogInWindows();
 }
 
+// Kontrollerar om det är redan inloggat
 function isLoggedIn() {
-  currentUser = localStorage.getItem("user" || null);
+  currentUser = localStorage.getItem("user");
   if (currentUser) {
     message.textContent = `Välkommen ${currentUser}, du är nu inloggad.`;
     logInBtn.textContent = "Logga ut";
@@ -71,15 +79,25 @@ function init() {
   closeBtn = document.getElementById("closeBtn");
   logInForm = document.querySelector("form");
   message = document.getElementById("message");
-  newUser("Kalle", "qwe123");
-  newUser("Kal", "123");
+  formsMessage = logInForm.querySelector("span");
 
+  // Skapar nya användare
+  newUser("Kalle", "qwe123");
+  newUser("Anna", "asd123");
+
+  // När sidan laddas kollar om det är redan inloggat
   isLoggedIn();
 
-  logInBtn.addEventListener("click", function () {
+  logInBtn.addEventListener("click", function (event) {
+    event.preventDefault();
     isLoggedIn() ? logOut() : showLogInWindows();
   });
+
   closeBtn.addEventListener("click", function () {
+    hideLogInWindows();
+  });
+
+  overlay.addEventListener("click", function () {
     hideLogInWindows();
   });
 
